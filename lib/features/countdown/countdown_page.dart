194513d3eb5ff3bd3d451cb32881a622f09d62bd
Dav418 <47563972+Dav418@users.noreply.github.com>
2026-05-29 00:15:48 +0100

    small remove

--- lib/features/countdown/countdown_page.dart
+++ lib/features/countdown/countdown_page.dart
@@ -1,16 +1,9 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
-import '../../domain/countdown_contact.dart';
 import '../../router/app_router.gr.dart';
 import '../../theme/app_typography.dart';
-import '../../widgets/gold_heart_rule.dart';
 import '../../widgets/wedding_countdown.dart';
-
-const _contacts = [
-  CountdownContact(name: 'Alisha', phone: '07712 345678'),
-  CountdownContact(name: 'Dawid', phone: '07925 456789'),
-];
 
 @RoutePage()
 class CountdownPage extends StatelessWidget {
@@ -28,17 +21,6 @@ class CountdownPage extends StatelessWidget {
       children: [
         const _CountdownScriptTitle(),
         const WeddingCountdown(showTitle: false),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(24, 28, 24, 0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const GoldHeartRule(),
-              const SizedBox(height: 28),
-              const _CountdownContactSection(),
-            ],
-          ),
-        ),
       ],
     );
   }
@@ -82,34 +64,6 @@ class _CountdownScriptTitle extends StatelessWidget {
           );
         },
       ),
-    );
-  }
-}
-
-class _CountdownContactSection extends StatelessWidget {
-  const _CountdownContactSection();
-
-  @override
-  Widget build(BuildContext context) {
-    final scheme = Theme.of(context).colorScheme;
-
-    return Column(
-      children: [
-        Text(
-          'CONTACT US',
-          textAlign: TextAlign.center,
-          style: AppTypography.sectionCaps(scheme, fontSize: 13, letterSpacing: 2),
-        ),
-        const SizedBox(height: 16),
-        for (var i = 0; i < _contacts.length; i++) ...[
-          Text(
-            '${_contacts[i].name}: ${_contacts[i].phone}',
-            textAlign: TextAlign.center,
-            style: AppTypography.contactLine(scheme),
-          ),
-          if (i < _contacts.length - 1) const SizedBox(height: 6),
-        ],
-      ],
     );
   }
 }
