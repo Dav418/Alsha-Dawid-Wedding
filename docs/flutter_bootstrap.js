557e34abe8080c47a44d15268108862ff81f331b
Dav418 <47563972+Dav418@users.noreply.github.com>
2026-06-22 12:55:35 +0100

    DEPLOY regenerated Flutter web app for custom domain

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "149476644"
+    serviceWorkerVersion: "2088640174"
   }
 });
