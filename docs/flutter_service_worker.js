31cb9e539e8f417dc7491ff803466529ccf259bd
Dav418 <47563972+Dav418@users.noreply.github.com>
2026-08-31 11:39:44 +0100

    DEPLOY regenerated Flutter web app for custom domain

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,13 +3,13 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "cbb3417a476f15cfdf93c10a246ebf82",
+const RESOURCES = {"flutter_bootstrap.js": "598e309b6075ccd7cad9e087be058437",
 "version.json": "c2159cf6dc2d4e150a27f9056d1543ff",
 "index.html": "8af5cf813dfa46fc46ffd6051ad1866a",
 "/": "8af5cf813dfa46fc46ffd6051ad1866a",
-"main.dart.js": "d6ef928275dfceb12a0ec16e2d140ea9",
+"main.dart.js": "b115ebf893d89bbb007fde06fc1b0c74",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"favicon.png": "f4e2fd0b45fd919b6a933109d05fa764",
+"favicon.png": "e010b46cca1ab6b80b3a4de5a2ae3636",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
